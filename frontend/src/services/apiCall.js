8a9bb51692a645dd9981fda7d21a9419dfd2a4dd
Dhruvin Dankhara <dh793203@dal.ca>
2023-07-31 03:50:45 -0300

    update frontend

--- frontend/src/services/apiCall.js
+++ frontend/src/services/apiCall.js
@@ -1,34 +1,19 @@
 import axios from "axios";
-// const BASE_URL = "http://ec2-52-206-22-233.compute-1.amazonaws.com:3000";
-const BASE_URL = "http://localhost:3000";
-export const fetchTodoApiCall = async () => {
-    try {
-        const response = await axios.get(
-            `${BASE_URL}/todos?userId=${localStorage.getItem("userId")}`
-        );
-        return response.data.data;
-    } catch (error) {
-        console.error(error);
-        return {};
-    }
-};
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcryptjs");
 
-export const updateStatus = async (id, status) => {
-    try {
-        const response = await axios.put(`${BASE_URL}/todo?todoId=${id}`, {
-            status: status,
-        });
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        return {};
-    }
-};
+// const BASE_URL = "http://ec2-52-206-22-233.compute-1.amazonaws.com:3000";
+const BASE_URL = "https://l67uggec83.execute-api.us-east-1.amazonaws.com/dev";
 
 // DONE
 export const loginApiCall = async (data) => {
     try {
-        const response = await axios.post(`${BASE_URL}/login`, data);
+        const payload = {
+            ...data,
+            // password: await bcrypt.hash(data.password, 10),
+            // TODO: 
+        };
+        const response = await axios.post(`${BASE_URL}/login`, payload);
         console.log(response.data);
         return response.data;
     } catch (error) {
@@ -40,7 +25,12 @@ export const loginApiCall = async (data) => {
 // DONE
 export const signUpApiCall = async (data) => {
     try {
-        const response = await axios.post(`${BASE_URL}/sign-up`, data);
+        const payload = {
+            ...data,
+            userId: uuidv4(),
+            // password: await bcrypt.hash(data.password, 10),
+        };
+        const response = await axios.post(`${BASE_URL}/sign-up`, payload);
         console.log(response.data);
         return response.data;
     } catch (error) {
@@ -49,9 +39,39 @@ export const signUpApiCall = async (data) => {
     }
 };
 
+export const fetchTodoApiCall = async () => {
+    try {
+        const response = await axios.get(
+            `${BASE_URL}/todo?userId=${localStorage.getItem("userId")}`
+        );
+        return response.data.data;
+    } catch (error) {
+        console.error(error);
+        return {};
+    }
+};
+
+export const updateStatus = async (id, status) => {
+    try {
+        console.log(id, status);
+        const response = await axios.put(
+            `${BASE_URL}/todo/update?todoId=${id}`,
+            {
+                status: status,
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return {};
+    }
+};
+
 export const deleteTodo = async (id) => {
     try {
-        const response = await axios.delete(`${BASE_URL}/delete?todoId=${id}`);
+        const response = await axios.delete(
+            `${BASE_URL}/todo/delete?todoId=${id}`
+        );
         return response.data;
     } catch (error) {
         console.error(error);
@@ -61,7 +81,11 @@ export const deleteTodo = async (id) => {
 
 export const createTodo = async (data) => {
     try {
-        const response = await axios.post(`${BASE_URL}/create-todo`, data);
+        const payload = {
+            ...data,
+            id: uuidv4(),
+        };
+        const response = await axios.post(`${BASE_URL}/todo/create`, payload);
         console.log(response);
         return true;
     } catch (error) {
